3cee5c76aba272b931bd165ea65a56bfacaa94c5
Ashish_Amar_Gaikwad <ashishgaikwad@accucia.co>
2023-05-07 22:25:06 +0530

    improve screen reponsive

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -4,13 +4,13 @@ const Card = (props) => {
     const { title, description, price, image } = props.prodData;
     const limitedDescription = description.slice(0, 50) + "...";
 
-    return <div className="bg-white shadow-lg rounded-lg overflow-hidden h-auto w-72 " >
+    return <div className="bg-white shadow-lg rounded-lg overflow-hidden h-auto w-72 mx-auto" >
         <img className="w-full h-48 object-contain" src={image} alt="Shopping item" />
         <div className="p-4">
             <h2 className="text-gray-900 font-bold text-xl mb-2">{title}</h2>
             <p className="text-gray-700 text-base"> {limitedDescription} </p>
             <div className="flex items-center justify-between mt-4">
-                <h3 className="text-gray-700 font-bold text-xl">Rs {price}</h3>
+                <h3 className="text-gray-700 font-bold text-xl">Rs {price * 75}</h3>
                 <Button className="bg-zinc-800 hover:bg-zinc-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Buy Now</Button>
             </div>
         </div>
--- src/components/cards/CardDetail.js
+++ src/components/cards/CardDetail.js
@@ -9,81 +9,81 @@ import Card from "./Card";
 import { v4 as uuidv4 } from "uuid";
 
 const CardDetail = () => {
-    const [productDetail, seProductDetail] = useState({});
-    const relatedProducts = useRelatedProducts(productDetail);
-    const { id } = useParams();
-    const { title, image, description, price } = productDetail;
-    const { addItemToCart } = useContext(Cartcontext);
+	const [productDetail, seProductDetail] = useState({});
+	const relatedProducts = useRelatedProducts(productDetail);
+	const { id } = useParams();
+	const { title, image, description, price } = productDetail;
+	const { addItemToCart } = useContext(Cartcontext);
 
-    useEffect(() => {
-        const product = PRODUCTS?.find((product) => product.id === +id);
-        seProductDetail(product);
-    }, [id]);
+	useEffect(() => {
+		const product = PRODUCTS?.find((product) => product.id === +id);
+		seProductDetail(product);
+	}, [id]);
 
-    const handleAddCart = () => {
-        const newProduct = {
-            ...productDetail,
-            id: uuidv4(),
-        };
-        addItemToCart(newProduct);
-    };
+	const handleAddCart = () => {
+		const newProduct = {
+			...productDetail,
+			id: uuidv4(),
+		};
+		addItemToCart(newProduct);
+	};
 
-    return (
-        <div className="container mx-auto px-4 py-4">
-            <div className="flex flex-wrap justify-between items-center">
-                <div className="w-full md:w-1/2 lg:w-3/5">
-                    <img src={image} className="w-full h-96 object-contain" alt={title} />
-                </div>
+	return (
+		<div className="container mx-auto px-4 py-4">
+			<div className="flex flex-wrap justify-between items-center">
+				<div className="w-full md:w-1/2 lg:w-3/5">
+					<img src={image} className="w-full h-96 object-contain" alt={title} />
+				</div>
 
-                <div className="w-full md:w-1/2 lg:w-2/5 mt-4 md:mt-0">
-                    <h1 className="text-2xl font-bold mb-2"> {title} </h1>
-                    <p className="text-gray-700 text-base mb-4"> {description}</p>
-                    <p className="text-lg font-bold mb-2">Rs {price} </p>
-                    <Button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={handleAddCart}
-                    >
-                        Add to Cart
-                    </Button>
+				<div className="w-full md:w-1/2 lg:w-2/5 mt-4 md:mt-0">
+					<h1 className="text-2xl font-bold mb-2"> {title} </h1>
+					<p className="text-gray-700 text-base mb-4"> {description}</p>
+					<p className="text-lg font-bold mb-2">Rs {price} </p>
+					<Button
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+						onClick={handleAddCart}
+					>
+						Add to Cart
+					</Button>
 
-                    <Link to={"/cart"}>
-                        <Button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mx-4">
-                            Go To Cart
-                        </Button>
-                    </Link>
-                </div>
-            </div>
+					<Link to={"/cart"}>
+						<Button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mx-4">
+							Go To Cart
+						</Button>
+					</Link>
+				</div>
+			</div>
 
-            {/* Related Products */}
-            <div className="my-5 ml-4">
-                <hr className="my-8 border-b-2 border-gray-300" />
-                <h2 className="text-4xl font-bold mb-4 text-green-500 text-center ">
-                    Releted Products
-                </h2>
+			{/* Related Products */}
+			<div className="my-5 ml-4">
+				<hr className="my-8 border-b-2 border-gray-300" />
+				<h2 className="text-4xl font-bold mb-4 text-green-500 text-center ">
+					Releted Products
+				</h2>
 
-                <div className="flex gap-2 mt-5 flex-wrap">
-                    {relatedProducts?.map((product) => (
-                        <Link to={"/card-detail/" + product.id} key={product?.id}>
-                            <Card prodData={product} />
-                        </Link>
-                    ))}
-                </div>
-            </div>
+				<div className="flex gap-2 mt-5 flex-wrap">
+					{relatedProducts?.map((product) => (
+						<Link to={"/card-detail/" + product.id} key={product?.id}>
+							<Card prodData={product} />
+						</Link>
+					))}
+				</div>
+			</div>
 
-            {/*  customer reviews */}
-            <div className="my-5 ml-4">
-                <hr className="my-8 border-b-2 border-gray-300" />
-                <h2 className="text-4xl font-bold mb-4 text-green-500 text-center ">
-                    Customer Reviews
-                </h2>
-                <div className="flex gap-4 mt-5 flex-wrap">
-                    {productDetail?.reviews?.map((product) => {
-                        return <Reviews reviewsData={product} key={product?.id} />;
-                    })}
-                </div>
-            </div>
-        </div>
-    );  
+			{/*  customer reviews */}
+			<div className="my-5 ml-4">
+				<hr className="my-8 border-b-2 border-gray-300" />
+				<h2 className="text-4xl font-bold mb-4 text-green-500 text-center ">
+					Customer Reviews
+				</h2>
+				<div className="flex gap-4 mt-5 flex-wrap">
+					{productDetail?.reviews?.map((product) => {
+						return <Reviews reviewsData={product} key={product?.id} />;
+					})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default CardDetail;
